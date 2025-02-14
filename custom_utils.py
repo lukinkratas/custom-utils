14c90ecad6498cd8f0ae2a08bfb43725e9afb970
Lukas Kratochvila <lukas.kratochvila@datasentics.com>
2025-02-14 13:02:42 +0100

    init

---
+++ custom_utils.py
@@ -0,0 +1,44 @@
+from functools import wraps
+import os
+import pwd
+from datetime import datetime
+from time import perf_counter
+
+# track decorator
+# time decorator
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
+
+def track_args(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        print(f"{datetime.now()} {get_username()} called {func.__name__} with {args=} and {kwargs=}.")
+
+        result = func(*args, **kwargs)
+
+        print(f"{func.__name__} finished successfully.")
+
+        return result
+    
+    return wrapper
+
+def track_time(func, n=1):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        print(f"{func.__name__} running {n}time(s) started.")
+        start_time = perf_counter()
+
+        for _ in range(n):
+            result = func(*args, **kwargs)
+
+        elapsed_time = perf_counter() - start_time
+        print(f"{func.__name__} finished, took: {elapsed_time:0.4f} seconds.")
+
+        return result
+    
+    return wrapper
