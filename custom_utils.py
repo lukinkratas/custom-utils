9e6fab125263d8aad2fa609d11850ea0ddc52e04
Lukas Kratochvila <lukas.kratochvila@datasentics.com>
2025-03-22 16:08:50 +0100

    lint and format

--- custom_utils.py
+++ custom_utils.py
@@ -1,107 +1,98 @@
-import pandas as pd
-from functools import wraps
 import os
 import pwd
 from datetime import datetime
-from time import perf_counter
-import boto3
-from botocore.exceptions import ClientError
+from functools import wraps
 from io import BytesIO
+from time import perf_counter
+
+import boto3
+import pandas as pd
+from botocore.exceptions import ClientError
 
 s3_client = boto3.client('s3')
+
 
 def get_username():
     return pwd.getpwuid(os.getuid())[0]
 
-def track_args(func):
 
+def track_args(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-
-        print(f"{datetime.now()} {get_username()} called {func.__name__} with {args=} and {kwargs=}.")
+        print(
+            f'{datetime.now()} {get_username()} called {func.__name__}\n  with {args=}\n  and {kwargs=}.'
+        )
 
         result = func(*args, **kwargs)
 
-        print(f"{func.__name__} finished successfully.")
+        print(f'{func.__name__} finished successfully.')
 
         return result
-    
+
     return wrapper
 
+
 def track_time_performance(n=1):
-
     def decorator(func):
-
         @wraps(func)
         def wrapper(*args, **kwargs):
-
-            print(f"{func.__name__} running {n}time(s) started.")
+            print(f'{func.__name__} running {n}time(s) started.')
             start_time = perf_counter()
 
             for _ in range(n):
                 result = func(*args, **kwargs)
 
             elapsed_time = perf_counter() - start_time
-            print(f"{func.__name__} finished, took: {elapsed_time:0.8f} seconds.")
+            print(f'{func.__name__} finished, took: {elapsed_time:0.8f} seconds.')
 
             return result
-        
+
         return wrapper
-    
+
     return decorator
 
-def s3_put_object(bytes, bucket:str, key_name:str):
 
+def s3_put_object(bytes, bucket: str, key_name: str):
     try:
-        response = s3_client.put_object(
-            Body=bytes,
-            Bucket=bucket,
-            Key=key_name
-        )
+        response = s3_client.put_object(Body=bytes, Bucket=bucket, Key=key_name)
 
     except ClientError as e:
         print(e)
         return False
-    
+
     return response
 
-def s3_put_df(df:pd.DataFrame, bucket_name:str, key_name:str, **kwargs):
 
+def s3_put_df(df: pd.DataFrame, bucket_name: str, key_name: str, **kwargs):
     bytes = BytesIO()
     df.to_parquet(bytes, **kwargs)
     bytes.seek(0)
     return s3_put_object(bytes.getvalue(), bucket_name, key_name)
 
-def s3_list_objects(bucket_name:str, key_prefix:str=''):
-    
+
+def s3_list_objects(bucket_name: str, key_prefix: str = ''):
     try:
-        response = s3_client.list_objects_v2(
-            Bucket=bucket_name,
-            Prefix=key_prefix
-        )
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=key_prefix)
 
     except ClientError as e:
         print(e)
         return False
-    
+
     return [content.get('Key') for content in response.get('Contents')]
 
-def s3_get_object(bucket_name:str, key_name:str):
-    
+
+def s3_get_object(bucket_name: str, key_name: str):
     try:
-        response = s3_client.get_object(
-            Bucket=bucket_name,
-            Key=key_name
-        )
+        response = s3_client.get_object(Bucket=bucket_name, Key=key_name)
 
     except ClientError as e:
         print(e)
         return False
-    
+
     return response
 
-def s3_read_df(bucket_name:str, key_name:str, **kwargs) -> pd.DataFrame:
 
+def s3_read_df(bucket_name: str, key_name: str, **kwargs) -> pd.DataFrame:
     response = s3_get_object(bucket_name, key_name)
     bytes = BytesIO(response['Body'].read())
     bytes.seek(0)
